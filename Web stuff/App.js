d1abca564d9b9d22e2e483bde823b6b3616d7607
I D Neel <idkneel@gmail.com>
2023-09-10 00:11:08 -0400

    ported over old work

---
+++ Web stuff/App.js
@@ -0,0 +1,29 @@
+// JavaScript source code
+import "./App.css";
+import {
+	Navbar,
+	Header,
+	Features,
+	Download,
+	Subscribe,
+	Faq,
+	Footer,
+} from "./components";
+
+function App() {
+    return (
+        <>
+            <header className="header-bg">
+                <Navbar />
+                <Header />
+            </header>
+            <Features data-aos="fade-up" />
+            <Download />
+            <Subscribe />
+            <Faq />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
